fc8f51bd645331c2b868f68718659b0fcc55872a
Wellington Ferreira <wellington.alex@conexia.com.br>
2024-04-17 14:28:40 -0300

    Protegendo a SECRET_KEY

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m6(6^5kn+pamho#p&wcnjjbvvg1*x_%j2!==zrdwlf+rf$n_t)'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
